80fa1465c7f914068e814fca0a256102637b5a05
Ruei Yu <vip8209@yahoo.com.tw>
2018-12-17 23:51:50 +0800

    handing the auth token

--- src/app/auth/store/auth.reducers.ts
+++ src/app/auth/store/auth.reducers.ts
@@ -27,6 +27,11 @@ export function authReducer(
         token: null,
         authenticated: false
       };
+    case AuthActions.SET_TOKEN:
+      return {
+        ...state,
+        token: action.payload
+      };
     default:
       return state;
   }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -21,20 +21,20 @@ export class DataStorageService {
     //   params: new HttpParams().set('auth', token)
     //   // headers: headers
     // });
-    const req = new HttpRequest('PUT', 'https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {reportProgress: true});
+    const req = new HttpRequest('PUT', 'https://ng-recipe-book-cc53e.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {reportProgress: true});
     return this.httpClient.request(req);
   }
 
   getRecipes() {
     // this.httpClient.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json?auth=' + token)
-    this.httpClient.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', {
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-cc53e.firebaseio.com/recipes.json', {
       observe: 'body',
       responseType: 'json'
     })
       .map(
         (recipes) => {
           console.log(recipes);
-          for (let recipe of recipes) {
+          for (const recipe of recipes) {
             if (!recipe['ingredients']) {
               recipe['ingredients'] = [];
             }
