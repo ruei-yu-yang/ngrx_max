dec4a21b5a74ce68b2fd68f7d8e139a5ebd0392e
Ruei Yu <vip8209@yahoo.com.tw>
2018-12-19 01:06:34 +0800

    navigation as a Side Effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
+import { Router } from '@angular/router';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/mergeMap';
@@ -41,36 +42,34 @@ export class AuthEffects {
       ];
     });
 
-    @Effect()
-    authSignin = this.actions$
-      .ofType(AuthActions.TRY_SIGNIN)
-      .map((action: AuthActions.TrySignup) => {
-        return action.payload;
-      })
-      .switchMap((authData: { username: string; password: string }) => {
-        return fromPromise(
-          firebase
-            .auth()
-            .signInWithEmailAndPassword(
-              authData.username,
-              authData.password
-            )
-        );
-      })
-      .switchMap(() => {
-        return fromPromise(firebase.auth().currentUser.getIdToken());
-      })
-      .mergeMap((token: string) => {
-        return [
-          {
-            type: AuthActions.SIGNIN
-          },
-          {
-            type: AuthActions.SET_TOKEN,
-            payload: token
-          }
-        ];
-      });
+  @Effect()
+  authSignin = this.actions$
+    .ofType(AuthActions.TRY_SIGNIN)
+    .map((action: AuthActions.TrySignup) => {
+      return action.payload;
+    })
+    .switchMap((authData: { username: string; password: string }) => {
+      return fromPromise(
+        firebase
+          .auth()
+          .signInWithEmailAndPassword(authData.username, authData.password)
+      );
+    })
+    .switchMap(() => {
+      return fromPromise(firebase.auth().currentUser.getIdToken());
+    })
+    .mergeMap((token: string) => {
+      this.router.navigate(['/']);
+      return [
+        {
+          type: AuthActions.SIGNIN
+        },
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }
+      ];
+    });
 
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private router: Router) {}
 }
